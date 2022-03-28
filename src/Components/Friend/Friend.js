60a0f9e0da4fc6c7550b0446a2fab4dd8fb3b771
zobaerahmedzihad1 <zobaerahmedzihad02@gmail.com>
2022-03-29 02:26:36 +0600

    show data on friend component and set daynamic path

--- src/Components/Friend/Friend.js
+++ src/Components/Friend/Friend.js
@@ -1,13 +1,21 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
-const Friend = ({friend}) => {
-     // console.log(friend);
-     const {name, username, id} = friend;
+const Friend = ({ friend }) => {
+  const { name, username, id } = friend;
+
+  const nevigate = useNavigate();
+  const showFriendDetail = (id) => {
+    const path = `/friend/${id}`;
+    nevigate(path);
+  };
   return (
     <div className="App">
       <h1>This is friend component. </h1>
       <h2>Name : {name} </h2>
-      <button>{username} Id: {id} </button>
+      <button onClick={() => showFriendDetail(id)}>
+        {username} Id: {id}{" "}
+      </button>
     </div>
   );
 };
