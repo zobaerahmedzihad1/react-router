846ea493cb6e2d463cb630acde495f6affe010ff
zobaerahmedzihad1 <zobaerahmedzihad02@gmail.com>
2022-03-29 02:01:15 +0600

    Basic route setup complete.

--- src/App.js
+++ src/App.js
@@ -9,7 +9,12 @@ function App() {
   return (
     <div>
       <h1>React Router.</h1>
-     
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/friends" element={<Friends />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
     </div>
   );
 }
